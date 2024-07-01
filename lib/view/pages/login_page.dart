017b34caf828cad4c364faf82d9d62bb2733f3f9
epashiq <ashiqashi4715@gmail.com>
2024-07-01 13:35:38 +0530

    sign up page completed

--- lib/view/pages/login_page.dart
+++ lib/view/pages/login_page.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hrm_project/controller/provider/auth_provider.dart';
 import 'package:hrm_project/view/pages/sign_up_page.dart';
 
 class LoginPage extends ConsumerWidget {
-   LoginPage({super.key});
+  LoginPage({super.key});
 
   final emailController = TextEditingController();
 
   final passController = TextEditingController();
 
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20),
@@ -38,9 +39,12 @@ class LoginPage extends ConsumerWidget {
             const SizedBox(
               height: 16,
             ),
-            ElevatedButton(onPressed: () {
-              
-            }, child: const Text('submit')),
+            ElevatedButton(
+                onPressed: () {
+                  ref.read(authProvider.notifier).loginWithEmailAndPassword(
+                      emailController.text, passController.text, context);
+                },
+                child: const Text('submit')),
             const SizedBox(
               height: 16,
             ),
@@ -53,7 +57,7 @@ class LoginPage extends ConsumerWidget {
                   Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) =>  SignUpPage(),
+                        builder: (context) => SignUpPage(),
                       ));
                 },
                 child: const Text('create an account'))
